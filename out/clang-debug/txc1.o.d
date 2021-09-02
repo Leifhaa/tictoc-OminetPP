1b889ec3e449a541d8709add981cb3cb44da421c
Leifhaa <leifhaa@hotmail.com>
2021-09-02 16:15:07 +0200

    Pt 2.

---
+++ out/clang-debug/txc1.o.d
@@ -0,0 +1,4 @@
+out/clang-debug//txc1.o: txc1.cc \
+  C:/Ominet++/omnetpp-5.6.2/include\omnetpp.h
+
+C:/Ominet++/omnetpp-5.6.2/include\omnetpp.h:
---
+++ out/clang-release/txc2.o.d
@@ -0,0 +1,4 @@
+out/clang-release//txc2.o: txc2.cc \
+  C:/Ominet++/omnetpp-5.6.2/include\omnetpp.h
+
+C:/Ominet++/omnetpp-5.6.2/include\omnetpp.h:
